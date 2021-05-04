666b06f42aa3e06dbe2d51aa6f672eaab448df83
ajith-code <70834458+ajith-code@users.noreply.github.com>
2021-05-04 13:16:47 +0530

    Add files via upload

---
+++ oddeven.cpp
@@ -0,0 +1,36 @@
+
+#include <iostream>
+
+
+using namespace std;
+
+
+int main()
+
+{
+
+int a;
+    
+cout<<"Enter an number";
+    
+cin>>a;
+    
+if(a%2==0)
+    
+{
+    
+cout<<a<<"is a EVEN number";
+   
+ }
+    
+else
+{
+        
+cout<<a<<"is a ODD number";
+    
+}
+
+    
+return 0;
+
+}
